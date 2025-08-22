16f240878e6066d37f0f3e909b4fe2877b4aec76
Sego <devpulseit@gmail.com>
2025-08-22 15:39:27 +0200

    Delete dev

--- dev-frontend/src/vite-env.d.ts
+++
@@ -1,9 +0,0 @@
-/// <reference types="vite/client" />
-
-declare global {
-  interface Window {
-    ym?: (counterId: number, action: string, goal?: string, options?: any) => void;
-  }
-}
-
-export {};
